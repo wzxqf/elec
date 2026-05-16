3654c11fd110ad973a5c3f92646a84cb65f232df
dk <3186221878@qq.com>
2026-05-16 22:45:29 +0800

    清理项目目录

--- 文本提取/pdf_abstract_body/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Reusable PDF abstract/body extraction helpers."""
-
-from .tei_parser import ParsedPaper, parse_tei_xml
-from .text_cleaner import clean_text
-
-__all__ = ["ParsedPaper", "parse_tei_xml", "clean_text"]
